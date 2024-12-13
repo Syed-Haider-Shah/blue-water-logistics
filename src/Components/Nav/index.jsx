1af8a54f1aa9a17d50ed95ef928bc2e42155e474
Syed-Haider-Shah <haidershah123123123@gmail.com>
2024-12-13 15:18:04 +1100

    Pages set

---
+++ src/Components/Nav/index.jsx
@@ -0,0 +1,23 @@
+import Button from '../Button';
+import Logo from '../Logo';
+import SearchBar from '../SearchBar';
+import "./index.css";
+
+const Nav = () => {
+  return(
+  <>
+  <nav className='nav'>
+    <Logo image='/Logo3.png' cusStyle='logo2' height="60"/>
+    <div className='hide-on-mobile'>
+    <Button title='Hats' cusStyle='nav-btn' dest='/detail?search=hats&page=1'/>
+    <Button title='Swimsuits' cusStyle='nav-btn' dest='/detail?search=swimsuits&page=1'/>
+    <Button title='Sunglasses' cusStyle='nav-btn' dest='/detail?search=sunglasses&page=1'/>
+    <Button title='Sandals' cusStyle='nav-btn' dest='/detail?search=sandals&page=1'/>
+    <Button title='Towels' cusStyle='nav-btn' dest='/detail?search=towels&page=1'/>
+    </div>
+  </nav>
+  </>
+  )
+}
+
+export default Nav
