b3e6f4091e14318e99fcc356254cc00bb2363e89
Syed-Haider-Shah <haidershah123123123@gmail.com>
2024-12-21 21:49:05 +1100

    Footer added

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import "./index.css"
+const Footer = () => {
+  return(
+    <footer className='footer'>
+      <p1 className="footer-text">© 2024 All rights Reserved. Design by Blue Water Logistics</p1>
+      <div className='whatsapp'></div>
+    </footer>
+  )
+}
+
+export default Footer;
