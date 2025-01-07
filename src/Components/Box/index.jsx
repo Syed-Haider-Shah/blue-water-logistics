05747544411776874c3e470c263395820b130357
Syed-Haider-Shah <haidershah123123123@gmail.com>
2025-01-07 21:10:25 +1100

    Section 2 Box design changed, numbers removed

--- src/Components/Box/index.jsx
+++ src/Components/Box/index.jsx
@@ -3,7 +3,6 @@ import "./index.css"
 const Box = ({number, heading, text, children}) => {
   return (
     <div className='box'>
-      <div className='left'>{number}</div>
       <div className='right'>
         <div className='logo-box'>{children}</div>
         <h1 className='heading-box'>{heading}</h1>
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -8,13 +8,13 @@ const Home = () => {
     <div className='section-2'>
       <h1 className='heading'>Why Perfume Vending Machines?</h1>
       <div className='boxes'>
-        <Box number='01' heading='Fast ROI' text='Earn your investment back in 3 months or less. High profitable and steadily growing business'>
+        <Box heading='Fast ROI' text='Earn your investment back in 3 months or less. High profitable and steadily growing business'>
           <Lightbulb size={48}/>
           </Box>
-        <Box number='02' heading='Low Investment' text='Get started with a low investment and enjoy high quality'>
+        <Box heading='Low Investment' text='Get started with a low investment and enjoy high quality'>
           <BadgeDollarSign size={48}/>
         </Box>
-        <Box number='03' heading='Smart Automatic' text='Business without the involvement of employees'>
+        <Box heading='Smart Automatic' text='Business without the involvement of employees'>
           <Bot size={48}/>
         </Box>
       </div>
