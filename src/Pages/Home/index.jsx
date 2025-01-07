bffe2064e47daaf1217159014466c3a8fc756158
Syed-Haider-Shah <haidershah123123123@gmail.com>
2025-01-07 21:59:08 +1100

    Section 3 and 4  content modified

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,5 +1,5 @@
 import { Box } from '../../Components';
-import { Lightbulb, BadgeDollarSign, Bot } from 'lucide-react';
+import { Lightbulb, BadgeDollarSign, Bot, ChartNoAxesCombined, Drill, Smile } from 'lucide-react';
 import "./index.css"
 const Home = () => {
   return (
@@ -8,26 +8,40 @@ const Home = () => {
     <div className='section-2'>
       <h1 className='heading'>Why Perfume Vending Machines?</h1>
       <div className='boxes'>
-        <Box heading='Fast ROI' text='Earn your investment back in 3 months or less. High profitable and steadily growing business'>
-          <Lightbulb size={48}/>
+        <Box heading='Customer Convenience' text='Offer a curated collection of premium perfumes, customized to align with customer preferences and current trends, enhancing the luxury and convenience of your venue&apos;s offerings.'>
+        <Smile size={48} />
           </Box>
-        <Box heading='Low Investment' text='Get started with a low investment and enjoy high quality'>
-          <BadgeDollarSign size={48}/>
+        <Box heading='Minimal Space' text='The machine occupies a small, visible area in high-traffic locations like restrooms or lobbies, ensuring maximum exposure with minimal space usage.'>
+          <Lightbulb size={48}/>
         </Box>
-        <Box heading='Smart Automatic' text='Business without the involvement of employees'>
+        <Box heading='Hassle Free' text='We manage installation, restocking, and maintenance, so your team can focus on what matters while we handle everything related to the vending machine.'>
           <Bot size={48}/>
+        </Box>
+      </div>
+      <div className='boxes'>
+        <Box heading='Contactless Payment' text='Convenient payments via mobile wallets, cards, and cash.'>
+          <BadgeDollarSign size={48}/>
+          </Box>
+        <Box heading='Potential Venue Benefits' text='Offers an additional amenity that aligns with your venue&apos;s premium image.'>
+          <ChartNoAxesCombined size={48}/>
+        </Box>
+        <Box heading='Inventory Management' text='Regular checks and replenishments to ensure a seamless experience.'>
+          <Drill size={48}/>
         </Box>
       </div>
     </div>
     <div className='section-3'>
       <div className='left-3'><img className="scaled-image-3"src="Vending.jpeg"/></div>
       <div className='right-3'>
-        <h1 className='heading-3'>Unlock Your Effortless Income</h1>
+        <h1 className='heading-3'>Product: Machine Specifications</h1>
         <ul className='all-dot-points'>
-          <li className='dot-point'><p1 className="bold">Profitability in less than 4 months</p1> – start seeing returns faster than ever.</li>
-          <li className='dot-point'><p1 className="bold">Up to 800% profit per use</p1> – maximize your revenue with each transaction.</li>
-          <li className='dot-point'><p1 className="bold">Full control at your fingertips</p1> – manage everything through our app, anytime, anywhere.</li>
-          <li className='dot-point'>Powered by a <p1 className="bold">one-of-a-kind global technology</p1> that sets you apart from the competition.</li>
+          <li className='dot-point'><p1 className="bold">Space Requirement: </p1>Minimal, thanks to its compact size.</li>
+          <li className='dot-point'><p1 className="bold">Dimensions: </p1>730mm x 200mm x 450mm</li>
+          <li className='dot-point'><p1 className="bold">Weight: </p1>22kg</li>
+          <li className='dot-point'><p1 className="bold">Electricity Consumption: </p1>Low, making it cost-efficient.</li>
+          <li className='dot-point'><p1 className="bold">Color:  </p1>Black, with a sleek and modern aesthetic.</li>
+          <li className='dot-point'><p1 className="bold">Operation: </p1>Plug-and-play installation with a user-friendly interface.</li>
+          <li className='dot-point'><p1 className="bold">Payment Options:  </p1>Supports contactless payments for convenience.</li>
         </ul>
       </div>
     </div>
